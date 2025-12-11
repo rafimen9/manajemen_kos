97f4992471cc6f97cf9e052e451a76ff53dfd28d
rafimen <19240459@bsi.ac.id>
2025-12-11 21:48:42 +0700

    form login

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('home');
+});
+
+Route::get('/login', function () {
+    return view('login');
 });
